1b689185578566c4a9cf59c60dc5c4b110d63ab2
TexHik <texhik620953@gmail.com>
2020-02-28 14:24:16 +0300

    done bfs-dfs

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -17,7 +18,7 @@ void dfsRecursive(vector<vector<int>> graph, int v = 0)
 			dfsRecursive(graph, graph[v][i]);
 }
 
-void bfs(vector<vector<int>> graph)
+void bfs(vector<vector<int>> graph, int start = 0)
 {
 	static vector<bool> visited;
 	if (visited.size() == 0)
@@ -25,29 +26,74 @@ void bfs(vector<vector<int>> graph)
 			visited.push_back(false);
 
 	queue<int> v;
-	for (int i = 0; i < graph[0].size(); i++)
-		v.push(graph[0][i]);
-	visited[0] = true;
+	vector<int> distance(graph.size());
+	distance[start] = 0;
+
+	for (int i = 0; i < graph[start].size(); i++)
+	{
+		v.push(graph[start][i]);
+		distance[i]++;
+	}
+	visited[start] = true;
+
 	while (v.size())
 	{
 		int temp = v.front();
 		v.pop();
+
+		for (int i = 0; i < graph[temp].size(); i++)
+			if (!visited[graph[temp][i]])
+			{
+				distance[graph[temp][i]] = distance[temp] + 1;
+				v.push(graph[temp][i]);
+			}
+
+		if (visited[temp] == false)
+			cout << temp << " ";
+		visited[temp] = true;
+	}
+	cout << endl;
+	for (int i = 0; i < distance.size(); i++)
+		cout << i << " - " << distance[i] << endl;
+}
+
+void dfs(vector<vector<int>> graph, int start = 0)
+{
+	static vector<bool> visited;
+	if (visited.size() == 0)
+		for (int i = 0; i < graph.size(); i++)
+			visited.push_back(false);
+
+	stack<int> v;
+	for (int i = 0; i < graph[start].size(); i++)
+		v.push(graph[start][i]);
+	visited[start] = true;
+	cout << start<<" ";
+	while (v.size())
+	{
+		int temp = v.top();
+		v.pop();
 		for (int i = 0; i < graph[temp].size(); i++)
 			if (!visited[graph[temp][i]])
 				v.push(graph[temp][i]);
+		if (visited[temp] == false)
+			cout << temp << " ";
 		visited[temp] = true;
 	}
-
 }
 
 int main()
 {
-	vector<vector<int>> graph = vector<vector<int>>(5);
+	vector<vector<int>> graph = vector<vector<int>>(7);
 	graph[0] = { 1,2,3,4 };
 	graph[1] = { 0,2,4 };
-	graph[2] = { 0,1 };
+	graph[2] = { 0,1,5,6 };
 	graph[3] = { 0,4 };
 	graph[4] = { 0,1,3 };
-	dfsRecursive(graph);
-	bfs(graph);
+	graph[5] = { 2,6 };
+	graph[6] = { 2,5 };
+	cout << "DFS: ";
+	dfs(graph);
+	cout << "\nBFS: ";
+	bfs(graph, 0);
 }
